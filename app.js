e39033eccb4406cb7e680b57757975642a8400a2
Jazmmine <jazmine.angoma@gmail.com>
2016-09-05 14:02:34 -0500

    subiendo

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ window.addEventListener("load", function() {
 		var tc = parseFloat(document.getElementById("tc").value);
 
 		var resultado = document.getElementById("resultado");
+		
+	
+
+	if (! isNaN(soles) && ! isNaN(tc)) {
 		resultado.innerHTML = "<p>El resultado es " + conversion(soles,tc) + "</p>";
-	});
+	} else {
+		resultado.innerHTML = "Ingrese número";
+	}
+
+   });
 
 	function conversion(soles, tc) {
 		return (soles/tc).toFixed(2);
